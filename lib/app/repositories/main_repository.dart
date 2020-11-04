5bd75de32f323c9cee25aa4c6f1ccfee2c6d76a9
Fernando Júnior <fernando.junior@tgcore.com.br>
2020-11-04 17:56:08 -0300

    delete repository content

--- lib/app/repositories/main_repository.dart
+++ lib/app/repositories/main_repository.dart
@@ -1,6 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:novo/app/models/pokemons_model.dart';
-import 'package:novo/app/shared/constants.dart';
 
 class MainRepository {
   MainRepository(this.dio);
